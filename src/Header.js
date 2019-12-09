9cd9d84f9d79154cd5a58adc198bceae82b34bee
Mahajan Chirayu Shrikant (IFIN DES TCP EDS) <ChirayuShrikant.Mahajan@infineon.com>
2019-12-09 19:05:28 +0530

    initial commit

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./header.css";
+const Header = () => {
+  return (
+    <div className="header">
+      <center>
+        <h2>My App</h2>
+      </center>
+    </div>
+  );
+};
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "./Header";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+  return (
+    <div className="container">
+      <Header />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+    </div>
+  );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById("root"));
